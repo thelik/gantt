32b2207354b3ece5a68b6b7aaec3d4059f2797b7
Like <thelik@163.com>
2020-08-10 14:03:03 +0800

    抽离通用模块

--- gantt.js
+++ gantt.js
@@ -1,20 +1,3 @@
-var demoData = [
-  { id: 59, pName: '项目1', name: '任务1', startTime: '2020-06-18 15:38:00', endTime: '2020-06-20 16:11:00' }
-  , { id: 37, pName: '项目1', name: '任务2', startTime: '2020-05-31 17:08:00', endTime: '2020-06-22 16:27:00' }
-  , { id: 44, pName: '项目1', name: '任务3', startTime: '2020-06-04 17:37:00', endTime: '2020-06-14 08:41:00' }
-  , { id: 54, pName: '项目2', name: '任务4', startTime: '2020-06-13 07:03:00', endTime: '2020-06-18 07:25:00' }
-  , { id: 58, pName: '项目2', name: '任务5', startTime: '2020-06-17 15:05:00', endTime: '2020-06-22 08:06:00' }
-  , { id: 50, pName: '项目3', name: '任务6', startTime: '2020-06-08 17:14:00', endTime: '2020-06-18 07:25:00' }
-  , { id: 55, pName: '项目3', name: '任务7', startTime: '2020-06-13 11:23:00', endTime: '2020-06-15 10:33:00' }
-  , { id: 57, pName: '项目3', name: '任务8', startTime: '2020-06-17 06:17:00', endTime: '2020-06-18 05:59:00' }
-  , { id: 53, pName: '项目3', name: '任务9', startTime: '2020-06-12 01:23:00', endTime: '2020-06-18 06:05:00' }
-  , { id: 56, pName: '项目3', name: '任务10', startTime: '2020-06-17 04:24:00', endTime: '2020-06-18 06:05:00' }
-  , { id: 52, pName: '项目4', name: '任务11', startTime: '2020-06-11 11:59:00', endTime: '2020-06-11 17:59:00' }
-  , { id: 1, pName: '项目4', name: '任务12', startTime: '2020-06-10 18:24:00', endTime: '2020-06-11 11:59:00' }
-  , { id: 43, pName: '项目5', name: '任务13', startTime: '2020-06-03 13:28:00', endTime: '2020-06-09 11:58:00' }
-]
-
-renderGantt(demoData)
 
 // 渲染甘特图
 function renderGantt(CharData) {
@@ -136,6 +119,7 @@ function renderGantt(CharData) {
     $('.GT-body-box').append(str)
   }
 }
+
 
 function getAll(begin, end) {
   var ab = begin.split("-");
